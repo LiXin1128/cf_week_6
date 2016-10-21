62fc43614b6451785bb6f183f86e2df7518ccfde
james <james@lemorelab.com>
2016-10-21 15:39:28 -0700

    10/21

---
+++ program2.rb
@@ -0,0 +1,6 @@
+if (5+5 == 10)
+	puts "This is true"
+else
+	puts "This is false"
+end
+		
---
+++ program4.rb
@@ -0,0 +1,13 @@
+3.times do 
+	puts "Good morning!"
+end
+
+number = 0
+while (number <= 10)
+	puts "the bumber is now #{number}" 
+	number += 1 #number = number +1
+end
+
+(0..10).each do |n|
+	p "the new number is #{n}"
+end
